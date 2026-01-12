b87ae5ccfc43c1f93afec5e50f07f691796a498e
GuillaumeLambermont <guillaume.lambermont@gmail.com>
2026-01-12 12:22:47 +0100

    Github actions setup

---
+++ src/main/java/be/corsac/legion/config/WebConfig.java
@@ -0,0 +1,22 @@
+package be.corsac.legion.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${ALLOWED_ORIGINS")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins(allowedOrigins) // Your Angular dev URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
--- src/main/java/be/corsac/legion/controllers/PlayerController.java
+++ src/main/java/be/corsac/legion/controllers/PlayerController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/players")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PlayerController {
 
     @Autowired
