7a01bb0452f21b8f6d05a2673cc09940565b8da9
Guillaume Lambermont <guillaume.lambermont@gmail.com>
2026-03-20 16:09:14 +0100

    Test fix mock jwt

--- src/test/java/be/corsac/legion/LegionApplicationTests.java
+++ src/test/java/be/corsac/legion/LegionApplicationTests.java
@@ -2,9 +2,14 @@ package be.corsac.legion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class LegionApplicationTests {
+
+	@MockitoBean // Use @MockBean if you are on an older Spring Boot version (< 3.4)
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
